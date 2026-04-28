a5b6569da64c4dc4fa0f5d46401d17803e832377
mohamed amine abdrrazek <mohamed-amine.abdrrazek@talan.com>
2026-04-28 10:46:48 +0100

    Refactor output variables in output.tf

--- output.tf
+++ output.tf
@@ -1,34 +1,47 @@
-output "s3_data_bucket" {
-  description = "S3 bucket for raw and processed data"
-  value       = aws_s3_bucket.data_lake.bucket
+output "data_lake_bucket" {
+  value = aws_s3_bucket.data_lake.bucket
 }
 
-output "rds_endpoint" {
-  description = "RDS PostgreSQL endpoint"
-  value       = aws_db_instance.postgres.endpoint
+output "raw_data_bucket" {
+  value = aws_s3_bucket.raw.bucket
 }
 
-output "rds_port" {
-  description = "RDS port"
-  value       = aws_db_instance.postgres.port
+output "processed_data_bucket" {
+  value = aws_s3_bucket.processed.bucket
 }
 
-output "glue_job_name" {
-  description = "AWS Glue ETL job name"
-  value       = aws_glue_job.etl_job.name
+output "rds_endpoint_main" {
+  value = aws_db_instance.main.endpoint
 }
 
-output "lambda_function_name" {
-  description = "Lambda function for ingestion"
-  value       = aws_lambda_function.ingestion.function_name
+output "rds_port_main" {
+  value = aws_db_instance.main.port
 }
 
-output "iam_role_arn" {
-  description = "IAM Role for data pipeline"
-  value       = aws_iam_role.data_pipeline_role.arn
+output "rds_endpoint_replica" {
+  value = aws_db_instance.replica.endpoint
 }
 
-output "sns_topic_arn" {
-  description = "SNS topic for alerts"
-  value       = aws_sns_topic.alerts.arn
+output "glue_ingestion_job" {
+  value = aws_glue_job.ingestion.name
+}
+
+output "glue_transformation_job" {
+  value = aws_glue_job.transformation.name
+}
+
+output "lambda_trigger_ingestion" {
+  value = aws_lambda_function.ingestion.function_name
+}
+
+output "emr_cluster_main" {
+  value = aws_emr_cluster.main.name
+}
+
+output "iam_role_pipeline" {
+  value = aws_iam_role.pipeline_role.arn
+}
+
+output "sns_alert_topic" {
+  value = aws_sns_topic.alerts.arn
 }
